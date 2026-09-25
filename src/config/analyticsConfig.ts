df481f7ab142aa07d77c6e20efa1783d38357fc8
Prajna-khadga <xunhe1984-droid@users.noreply.github.com>
2026-09-25 09:24:01 +0800

    feat: enable Google Analytics tracking

--- src/config/analyticsConfig.ts
+++ src/config/analyticsConfig.ts
@@ -2,7 +2,7 @@ import type { AnalyticsConfig } from "../types/analyticsConfig";
 
 export const analyticsConfig: AnalyticsConfig = {
 	// Google Analytics ID
-	googleAnalyticsId: "",
+	googleAnalyticsId: "G-XF178QJYTB",
 	// Microsoft Clarity ID
 	microsoftClarityId: "",
 	// Umami 统计配置
